33a4a1486765a3291e68ce1c6df6089079df2667
Matthieu <matthieunadeau96@gmail.com>
2020-05-13 17:47:38 -0400

    created a height selector with a function to translate inches to feet

--- lib/main.dart
+++ lib/main.dart
@@ -30,6 +30,24 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   bool male = false;
   bool female = false;
+  int age = 25;
+  int inches = 70;
+  int feet = 5;
+  int remainderInches = 10;
+
+  void inchesToFeet() {
+    setState(() {
+      double dec = (inches / 12);
+      feet = double.parse(dec.toStringAsFixed(1)).toInt();
+      int totalInches = feet * 12;
+      if (totalInches >= inches) {
+        remainderInches = totalInches - inches;
+      } else {
+        remainderInches = inches - totalInches;
+      }
+      print('$feet\'$remainderInches\"');
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -44,6 +62,18 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'BMI Calculator',
+                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 30,
+            ),
             Text('What is your biological gender?'),
             SizedBox(height: 10),
             Row(
@@ -75,11 +105,48 @@ class _MyHomePageState extends State<MyHomePage> {
               height: 30,
             ),
             Text('How old are you?'),
-            NumberPicker.horizontal(
-              initialValue: 20,
-              minValue: 5,
-              maxValue: 79,
-              onChanged: null,
+            SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                NumberPicker.horizontal(
+                  initialValue: age,
+                  minValue: 5,
+                  maxValue: 79,
+                  onChanged: (newValue) => setState(
+                    () => age = newValue,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Row(
+              children: [
+                Text('Height: '),
+                Text(
+                  '$feet\'$remainderInches\"',
+                  style: TextStyle(fontSize: 16, color: themeData.primaryColor),
+                ),
+              ],
+            ),
+            SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                NumberPicker.horizontal(
+                  initialValue: inches,
+                  minValue: 5,
+                  maxValue: 79,
+                  onChanged: (newValue) {
+                    setState(() {
+                      inches = newValue;
+                      inchesToFeet();
+                    });
+                  },
+                ),
+              ],
             ),
           ],
         ),
